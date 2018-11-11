064f8596aa3b21c5a02f7ab822501156a02adf93
Cody <callahanrts@gmail.com>
2018-11-11 09:25:16 -0800

    Only retrieve spotify track if the application is open
    fixes #5

--- index.jsx
+++ index.jsx
@@ -60,7 +60,7 @@ const result = (data, key) => {
 export const command = `
 BAT=$(pmset -g batt | egrep '([0-9]+\%).*' -o --colour=auto | cut -f1 -d';');
 SPACE=$(echo $(chunkc tiling::query -d id))
-SPOTIFY=$(osascript -e 'tell application "Spotify"	if player state is playing then		set artistName to artist of current track		set trackName to name of current track		return artistName & " - " & trackName	else		return ""	end ifend tell')
+SPOTIFY=$(osascript -e 'tell application "System Events"set processList to (name of every process)end tellif (processList contains "Spotify") is true thentell application "Spotify"if player state is playing thenset artistName to artist of current trackset trackName to name of current trackreturn artistName & " - " & trackNameelsereturn ""end ifend tellend if')
 
 
 echo $(cat <<-EOF
@@ -79,9 +79,10 @@ export const render = ({ output, error }) => {
     console.log(error)
     console.log(String(error))
   }
-  return error ? (
+  let errorContent = (
     <div style={barStyle}></div>
-  )  : (
+  )
+  let content = (
     <div style={barStyle}>
       <link rel="stylesheet" type="text/css" href="bar/assets/font-awesome/css/all.min.css" />
       <Workspaces config={config.workspaces} data={result(output, "workspace")} side="left" />
@@ -91,5 +92,6 @@ export const render = ({ output, error }) => {
       <Time config={config.time} side="right"></Time>
       <Battery config={config.battery} data={result(output, "battery")} side="right" />
     </div>
-  );
+  )
+  return error || !output ? errorContent : content
 }
